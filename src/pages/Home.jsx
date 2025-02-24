b2b0e0973b89753523232b8c6cbdfc94bdf00991
MKarthees <kartheeswaran68@gmail.com>
2025-02-24 12:45:12 -0800

    New Project Added Commit

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,84 +1,85 @@
-import React from "react";
-import "../css/Home.css";
-import { FaQuoteLeft } from "react-icons/fa6";
-import { FaCode, FaEdit } from "react-icons/fa";
-import watermarkLogo from "../assets/images/Color Logo.png";
-import projectVideo from "../assets/video/Project Show.mp4";
-import proOne from "../assets/video/1 P.mp4";
-import proTwo from "../assets/video/2 P.mp4";
-import proThree from "../assets/video/3 P.mp4";
+import React from 'react'
+import '../css/Home.css'
+import { FaQuoteLeft } from 'react-icons/fa6'
+import { FaCode, FaEdit } from 'react-icons/fa'
+import watermarkLogo from '../assets/images/Color Logo.png'
+import projectVideo from '../assets/video/Project Show.mp4'
+import proOne from '../assets/video/1 P.mp4'
+import proTwo from '../assets/video/2 P.mp4'
+import proThree from '../assets/video/3 P.mp4'
+import proRecent from '../assets/video/6 P.mp4'
 
-import { useNavigate } from "react-router-dom";
+import { useNavigate } from 'react-router-dom'
 
 const Home = () => {
-	const navigateBtn = useNavigate();
-	return (
-		<div id="home" className="home-container">
-			<section className="hero">
-				<h1>
-					Hi, I'm <span>Kartheeswaran</span>
-				</h1>
-				<h2>Developer & Designer</h2>
-				<p>
-					I develop and design modern web apps using the <span>MERN </span>
-					stack, blending technical skills with a passion for design.
-				</p>
-				<h3>
-					<i>
-						<FaQuoteLeft />
-					</i>
-					<span>Thinking </span>
-					only <span> leads </span>to
-					<span> creativity</span>
-				</h3>
-			</section>
+  const navigateBtn = useNavigate()
+  return (
+    <div id="home" className="home-container">
+      <section className="hero">
+        <h1>
+          Hi, I'm <span>Kartheeswaran</span>
+        </h1>
+        <h2>Developer & Designer</h2>
+        <p>
+          I develop and design modern web apps using the <span>MERN </span>
+          stack, blending technical skills with a passion for design.
+        </p>
+        <h3>
+          <i>
+            <FaQuoteLeft />
+          </i>
+          <span>Thinking </span>
+          only <span> leads </span>to
+          <span> creativity</span>
+        </h3>
+      </section>
 
-			<section className="hero-content">
-				<video type="video/mp4" autoPlay loop muted>
-					<source src={projectVideo} />
-				</video>
+      <section className="hero-content">
+        <video type="video/mp4" autoPlay loop muted>
+          <source src={projectVideo} />
+        </video>
 
-				<div className="card-show">
-					<div className="home-card">
-						<div className="card-title">
-							<span>
-								<FaCode />
-							</span>
-							<h4>Developer</h4>
-						</div>
-						<p>Developing with precision and innovation.</p>
-					</div>
-					<div className="home-card">
-						<div className="card-title">
-							<span>
-								<FaEdit />
-							</span>
-							<h4>Designer</h4>
-						</div>
-						<p>Turning ideas into captivating visuals.</p>
-					</div>
-				</div>
+        <div className="card-show">
+          <div className="home-card">
+            <div className="card-title">
+              <span>
+                <FaCode />
+              </span>
+              <h4>Developer</h4>
+            </div>
+            <p>Developing with precision and innovation.</p>
+          </div>
+          <div className="home-card">
+            <div className="card-title">
+              <span>
+                <FaEdit />
+              </span>
+              <h4>Designer</h4>
+            </div>
+            <p>Turning ideas into captivating visuals.</p>
+          </div>
+        </div>
 
-				<div className="project-cards">
-					<h1>Recent Works</h1>
-					<div className="project-img">
-						<video autoPlay loop muted>
-							<source src={proOne} />
-						</video>
-						<video autoPlay loop muted>
-							<source src={proTwo} />
-						</video>
-						<video autoPlay loop muted>
-							<source src={proThree} />
-						</video>
-					</div>
-					<button type="button" onClick={() => navigateBtn("project")}>
-						View All Project
-					</button>
-				</div>
-			</section>
-		</div>
-	);
-};
+        <div className="project-cards">
+          <h1>Recent Works</h1>
+          <div className="project-img">
+            <video autoPlay loop muted>
+              <source src={proTwo} />
+            </video>
+            <video autoPlay loop muted>
+              <source src={proRecent} />
+            </video>
+            <video autoPlay loop muted>
+              <source src={proOne} />
+            </video>
+          </div>
+          <button type="button" onClick={() => navigateBtn('project')}>
+            View All Project
+          </button>
+        </div>
+      </section>
+    </div>
+  )
+}
 
-export default Home;
+export default Home
--- src/pages/Project.jsx
+++ src/pages/Project.jsx
@@ -1,51 +1,56 @@
-import React from "react";
-import "../css/Project.css";
-import Card from "../components/Card";
+import React from 'react'
+import '../css/Project.css'
+import Card from '../components/Card'
 
-import projectOne from "../assets/video/1 P.mp4";
-import projectTwo from "../assets/video/2 P.mp4";
-import projectThree from "../assets/video/3 P.mp4";
-import projectFour from "../assets/video/4 P.mp4";
-import projectFive from "../assets/video/5 P.mp4";
+import projectOne from '../assets/video/1 D.mp4'
+import projectTwo from '../assets/video/2 D.mp4'
+import projectThree from '../assets/video/3 D.mp4'
+import projectFour from '../assets/video/4 D.mp4'
+import projectFive from '../assets/video/5 D.mp4'
+import projectSix from '../assets/video/6 D.mp4'
 const Project = () => {
-	return (
-		<div id="project" className="project-container">
-			<div className="show-cards">
-			<Card
-				name="Digital Clock & Stopwatch"
-				image={projectOne}
-				description="A digital clock (24-hour & 12-hour) and stopwatch built with HTML, CSS, and JavaScript, featuring real-time updates and a user-friendly interface."
-				highlight="HTML | CSS | JS"
-			/>
-			<Card
-				name="Counter App"
-				image={projectTwo}
-				description="A simple counter application built with HTML, CSS, and JavaScript, allowing users to increment, decrement, and reset values with ease."
-				highlight="HTML | CSS | JS"
-			/>
-			<Card
-				name="To-Do List App"
-				image={projectThree}
-				description="A simple to-do list app using HTML, CSS, and JavaScript for easy task management with a clean, responsive design."
-				highlight="HTML | CSS | JS"
-			/>
-			<Card
-				name="Quote Generator"
-				image={projectFour}
-				description="A simple quote generator using HTML, CSS, JavaScript, and Fetch API to display random quotes with a clean, responsive design."
-				highlight="HTML | CSS | JS"
-			/>
-			<Card
-				name="Weather Checker"
-				image={projectFive}
-				description="A weather-checking app built with HTML, CSS, JavaScript, and Fetch API. It provides real-time weather updates for any location with a clean, responsive interface."
-				highlight="HTML | CSS | JS"
-			/>
+  return (
+    <div id="project" className="project-container">
+      <div className="show-cards">
+        <Card
+          name="Work Day Training Portal"
+          image={projectSix}
+          description="The Work Day Training Portal website for Ramaussys Technologies uses HTML, CSS, JavaScript, and React to deliver a responsive, user-friendly website, enhancing usability and boosting the company’s online presence."
+          highlight="React | HTML | CSS | JavaScript"
+        />
+        <Card
+          name="Digital Clock & Stopwatch"
+          image={projectOne}
+          description="A digital clock (24-hour & 12-hour) and stopwatch built with HTML, CSS, and JavaScript, featuring real-time updates and a user-friendly interface."
+          highlight="HTML | CSS | JS"
+        />
+        <Card
+          name="Counter App"
+          image={projectTwo}
+          description="A simple counter application built with HTML, CSS, and JavaScript, allowing users to increment, decrement, and reset values with ease."
+          highlight="HTML | CSS | JS"
+        />
+        <Card
+          name="To-Do List App"
+          image={projectThree}
+          description="A simple to-do list app using HTML, CSS, and JavaScript for easy task management with a clean, responsive design."
+          highlight="HTML | CSS | JS"
+        />
+        <Card
+          name="Quote Generator"
+          image={projectFour}
+          description="A simple quote generator using HTML, CSS, JavaScript, and Fetch API to display random quotes with a clean, responsive design."
+          highlight="HTML | CSS | JS"
+        />
+        <Card
+          name="Weather Checker"
+          image={projectFive}
+          description="A weather-checking app built with HTML, CSS, JavaScript, and Fetch API. It provides real-time weather updates for any location with a clean, responsive interface."
+          highlight="HTML | CSS | JS"
+        />
+      </div>
+    </div>
+  )
+}
 
-			</div>
-		</div>
-
-	);
-};
-
-export default Project;
+export default Project
